70f79257adb55a0ad05b850bb5f298f4792d93ad
Max Staite <max.staite2004@gmail.com>
2023-06-10 15:23:46 +0100

    isolating with dummies

--- __tests__/Itinerary.test.js
+++ __tests__/Itinerary.test.js
@@ -1,6 +1,6 @@
 /* globals describe it expect */
 const Itinerary = require('../src/Itinerary.js');
-const Port = require('../src/Port.js');
+
 describe('Itinerary', () => {
     
     it('can be instantiated', () => {
@@ -8,8 +8,8 @@ describe('Itinerary', () => {
     });
 
     it('has a ports property', () => {
-        const gloucester = new Port('Gloucester');
-        const dover = new Port('Dover');
+        const gloucester = jest.fn();
+        const dover = jest.fn();
 
         const itinerary = new Itinerary([gloucester, dover]);
 
